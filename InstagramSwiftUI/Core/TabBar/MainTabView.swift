0dfa7193516a7c4db5aaa7fa463271eb4064ab2f
RustamKeneev <dr.rustamkeneev@gmail.com>
2024-05-19 22:15:31 +0600

    start setting main tabview

---
+++ InstagramSwiftUI/Core/TabBar/MainTabView.swift
@@ -0,0 +1,45 @@
+//
+//  MainTabView.swift
+//  InstagramSwiftUI
+//
+//  Created by Rustam Keneev on 19/5/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    //MARK: - PROPERTIES
+
+    //MARK: - BODY
+    var body: some View {
+        TabView{
+            Text("Home")
+                .tabItem {
+                    Image(systemName: "house")
+                }//: TAB ITEM HOME
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }//: TAB ITEM SEARCH
+            Text("Upload")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }//: TAB ITEM UPLOAD
+            Text("Notificcation")
+                .tabItem {
+                    Image(systemName: "heart")
+                }//: TAB ITEM NOTIFICATION
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }//: TAB ITEM PROFILE
+        }//: TABVIEW
+        .accentColor(Color.black)
+    }//: END BODY
+}//: END MAIN TAB VIEW
+
+
+//MARK: - PREVIEW
+#Preview {
+    MainTabView()
+}
